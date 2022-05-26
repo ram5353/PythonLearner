a628b511b31cf7704dd9fb8c093f4663b2633c7b
ram5353 <ramkiran5353@gmail.com>
2022-05-26 13:06:32 -0400

    Exception handling in generate phonotic

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -17,17 +17,20 @@ for (index, row) in student_data_frame.iterrows():
     #Access row.student or row.score
     pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+
 phonetic_words = {row.letter: row.code for (index, row) in data_frame.iterrows()}
 print(phonetic_words)
 
 
-#Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter the word").upper()
-result = [phonetic_words[letter] for letter in user_input]
-print(result)
+def generate_phonetic():
+    user_input = input("Enter the word: ").upper()
+    try:
+        result = [phonetic_words[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, enter only alphabets")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()
