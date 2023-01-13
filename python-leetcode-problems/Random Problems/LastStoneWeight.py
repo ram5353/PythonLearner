7a9fd334295567a0c5e8197005399df92afe2292
ram5353 <ramkiran5353@gmail.com>
2023-01-12 19:23:19 -0500

    Last stone weight heap problem

--- python-leetcode-problems/Random Problems/LastStoneWeight.py
+++ python-leetcode-problems/Random Problems/LastStoneWeight.py
@@ -2,6 +2,14 @@ import heapq
 
 
 def lastStoneWeight(stones):
+    stones = [-stones[i] for i in range(len(stones))]
     heapq.heapify(stones)
     while len(stones) > 1:
+        stone1 = heapq.heappop(stones)
+        stone2 = heapq.heappop(stones)
+        if stone1 != stone2:
+            heapq.heappush(stones, stone1-stone2)
+    return -heapq.heappop(stones) if stones else 0
+    
+print(lastStoneWeight([1]))
         
